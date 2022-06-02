8c860c03e4752520bdd8f802d2033b656540ac1d
Simon <simon.mravec@gmail.com>
2022-06-02 23:19:29 +0200

    Added install script

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
+#!/bin/sh
+
+#Tested only on Arch Linux, on other distros not guranteed to work
+
+function copy_theme_content(){
+	sudo cp ./Framework-Variant1 /usr/share/grub/themes/
+}
+
+function edit_grub_config(){
+	
+	sudo echo "GRUB_TIMEOUT_STYLE="menu"" >> /etc/default/grub
+	sudo echo "GRUB_THEME=/usr/share/grub/themes/Framework-Variant1/theme.txt" >> /etc/default/grub
+	sudo echo "GRUB_TIMEOUT="10"" >> /etc/default/grub
+
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+}
+
 echo "installing theme..."
 
-
+copy_theme_content
+edit_grub_config
 
 echo "theme installed"
