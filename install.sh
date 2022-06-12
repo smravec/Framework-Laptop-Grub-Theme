2a4bc69bdbc420c31254ba138810847a34247996
simon <simon.mravec@gmail.com>
2022-06-12 22:54:03 +0200

    Renamed some of the files

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 #Tested only on Arch Linux, on other distros not guranteed to work
 
 function copy_theme_content(){
-	sudo cp -r ./Framework-Variant1 /usr/share/grub/themes/
+	sudo cp -r ./framework-theme /usr/share/grub/themes/
 }
 
 function edit_grub_config(){
 	
-	sudo echo "GRUB_THEME=/usr/share/grub/themes/Framework-Variant1/theme.txt" >> /etc/default/grub
+	sudo echo "GRUB_THEME=/usr/share/grub/themes/framework-theme/theme.txt" >> /etc/default/grub
 
 	sudo grub-mkconfig -o /boot/grub/grub.cfg
 }
